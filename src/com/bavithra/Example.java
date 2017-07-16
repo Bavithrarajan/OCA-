9651afe0a6db977d6cf0f3450f9a5ea80d24303a
Elcot <Elcot@Elcot-PC>
2017-07-16 20:38:15 +0530

    Sample Codes

---
+++ src/com/bavithra/Example.java
@@ -0,0 +1,39 @@
+package com.bavithra;
+
+public class Example {
+
+	public static void main(String[] args) {
+		/*String name = "James";
+
+	      // following will return true since name is type of String
+	      boolean result = name instanceof String;
+	      System.out.println( result );
+	   }	
+		
+		// TODO Auto-generated method stub
+
+	}*/
+		/*int [] numbers = {10, 20, 30, 40, 50};
+
+	      for(int x : numbers ) {
+	         System.out.print( x );
+	         System.out.print(",");
+	      }
+	      System.out.print("\n");
+	      String [] names = {"James", "Larry", "Tom", "Lacy"};
+
+	      for( String name : names ) {
+	         System.out.print( name );
+	         System.out.print(",");
+	      }
+	   }*/
+		/*Integer x = 5; 
+	      x =  x + 10;   
+	      System.out.println(x); 
+	   }
+	}*/
+		char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+	      String helloString = new String(helloArray);  
+	      System.out.println( helloString );
+	   }
+	}
---
+++ src/com/bavithra/Test.java
@@ -0,0 +1,8 @@
+package com.bavithra;
+
+public class Test {
+public final static void main(String[]args){
+	double d = -22.22222;
+	System.out.println(Math.ceil(d));
+	}
+}
