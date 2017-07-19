ee823bd187fc4a774c34b416b49d49b00c3ea0c0
Bavithrarajan <bavithrarajan.be@gmail.com>
2017-07-19 12:13:51 +0530

    Add files via upload

---
+++ S.java
@@ -0,0 +1,88 @@
+package com.certification;
+/*class BlockTest{
+	public static void main(String[]args){
+		int x,y;
+		y=13;
+		for(x=0;x<10;x++)
+		{
+			System.out.println("The value is:"+x);
+			System.out.println("The value is:"+y);
+		}
+	}
+}*/
+/*class Area{
+	public static void main(String[]args){
+		double pi,r,a;
+		r=2.15;
+		pi=3.14;
+		a=pi*r*r;
+		System.out.println("The Area is:"+a);
+		
+	}
+}*/
+/*class Light{
+	public static void main(String[]args){
+		int lightspeed;
+		long days;
+		long seconds;
+		long distance;
+		lightspeed=186000;
+		days=1000;
+		seconds=days*24*60*60;
+		distance=lightspeed*seconds;
+		System.out.println("In"+days);
+		System.out.println("days light will travel about");
+		System.out.println(distance+"miles.");
+	}
+}*/
+/*class Modulus{
+	public static void main(String[]args){
+		int x =33;
+		double y = 33.25;
+		System.out.println("x mod 10 = "+x%10);
+		System.out.println("y mod 10 = "+y%10);
+	}
+}*/
+/*class Assignment{
+	public static void main(String[]args){
+		int a=4;
+		int b=5;
+		int c=3;
+		a+=4;
+		b*=2;
+		c%=1;
+		c+=a*b;
+		System.out.println(a + "  " + b + " " + c);
+	}
+}*/
+
+/*class S{
+  public static void main(String args[]){
+  int x = 10;
+  int y = new S().change(x);
+    System.out.print(x+y);
+  }
+  int change(int x){
+  x=12;
+  return x;
+  }
+  }
+*/
+
+/*class S{
+  public static void main(String [] args){
+  String s = "Java";
+  s.concat(" SE 6");
+  s.replace('6','7');
+  System.out.print(s);
+  }
+  }
+*/
+
+
+
+
+
+
+
+
